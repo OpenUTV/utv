cae0dcb67df40defce50da817c8cde4070c47fbe
Michael Oliver <mcoliver@gmail.com>
2026-04-30 19:38:57 -0700

    chore: Add runtime dependencies to Homebrew cask
    Signed-off-by: Michael Oliver <mcoliver@gmail.com>

--- Casks/utv.rb
+++ Casks/utv.rb
@@ -9,6 +9,24 @@ cask "utv" do
 
   app "UTV.app"
 
+  depends_on formula: "qt"
+  depends_on formula: "ffmpeg"
+  depends_on formula: "openexr"
+  depends_on formula: "imath"
+  depends_on formula: "opencolorio"
+  depends_on formula: "libraw"
+  depends_on formula: "libtiff"
+  depends_on formula: "libpng"
+  depends_on formula: "boost"
+  depends_on formula: "openimageio"
+  depends_on formula: "openjpeg"
+  depends_on formula: "webp"
+  depends_on formula: "yaml-cpp"
+  depends_on formula: "spdlog"
+  depends_on formula: "icu4c"
+  depends_on formula: "openjph"
+  depends_on formula: "jpeg-turbo"
+
   zap trash: [
     "~/Library/Preferences/com.OpenUTV.UTV.plist",
     "~/Library/Saved Application State/com.OpenUTV.UTV.savedState",
