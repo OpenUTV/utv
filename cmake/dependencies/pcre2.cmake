7e2143cd50f192c83631ad129e19c550fbd7c488
Michael Oliver <mcoliver@gmail.com>
2026-05-06 16:37:19 -0700

    fix: resolve CMake alias-to-alias restriction in vcpkg wrapper for pcre2
    Signed-off-by: Michael Oliver <mcoliver@gmail.com>

--- cmake/dependencies/pcre2.cmake
+++ cmake/dependencies/pcre2.cmake
@@ -8,20 +8,36 @@ FIND_PACKAGE(pcre2 CONFIG REQUIRED)
 
 IF(TARGET pcre2::pcre2-8)
   IF(NOT TARGET pcre2-8)
-    ADD_LIBRARY(pcre2-8 ALIAS pcre2::pcre2-8)
+    ADD_LIBRARY(pcre2-8 INTERFACE IMPORTED GLOBAL)
+    TARGET_LINK_LIBRARIES(
+      pcre2-8
+      INTERFACE pcre2::pcre2-8
+    )
   ENDIF()
 ELSEIF(TARGET PCRE2::8BIT)
   IF(NOT TARGET pcre2-8)
-    ADD_LIBRARY(pcre2-8 ALIAS PCRE2::8BIT)
+    ADD_LIBRARY(pcre2-8 INTERFACE IMPORTED GLOBAL)
+    TARGET_LINK_LIBRARIES(
+      pcre2-8
+      INTERFACE PCRE2::8BIT
+    )
   ENDIF()
 ENDIF()
 
 IF(TARGET pcre2::pcre2-posix)
   IF(NOT TARGET pcre2-posix)
-    ADD_LIBRARY(pcre2-posix ALIAS pcre2::pcre2-posix)
+    ADD_LIBRARY(pcre2-posix INTERFACE IMPORTED GLOBAL)
+    TARGET_LINK_LIBRARIES(
+      pcre2-posix
+      INTERFACE pcre2::pcre2-posix
+    )
   ENDIF()
 ELSEIF(TARGET PCRE2::POSIX)
   IF(NOT TARGET pcre2-posix)
-    ADD_LIBRARY(pcre2-posix ALIAS PCRE2::POSIX)
+    ADD_LIBRARY(pcre2-posix INTERFACE IMPORTED GLOBAL)
+    TARGET_LINK_LIBRARIES(
+      pcre2-posix
+      INTERFACE PCRE2::POSIX
+    )
   ENDIF()
 ENDIF()
