99146a50d05c02d352c83e5eeb1142896685d739
Michael Oliver <mcoliver@gmail.com>
2026-05-06 20:17:59 -0700

    fix: resolve CMake circular dependency by using absolute path for PACKAGE metadata files
    Signed-off-by: Michael Oliver <mcoliver@gmail.com>

--- cmake/macros/rv_stage.cmake
+++ cmake/macros/rv_stage.cmake
@@ -464,7 +464,7 @@ FUNCTION(rv_stage)
       ADD_CUSTOM_COMMAND(
         COMMENT "Creating ${_package_filename} ..."
         OUTPUT ${_package_filename}
-        DEPENDS ${_temp_file} ${_files} ${_package_file}
+        DEPENDS ${_temp_file} ${_files} "${CMAKE_CURRENT_SOURCE_DIR}/${_package_file}"
         COMMAND ${CMAKE_COMMAND} -E tar "cfv" ${_package_filename} --format=zip --files-from=${_temp_file}
         COMMAND ${CMAKE_COMMAND} -E rm -f ${RV_STAGE_PLUGINS_PACKAGES_DIR}/rvinstall
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
