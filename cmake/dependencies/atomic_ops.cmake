34b12130d9b71e59a1c8babcfb73986a48e54396
Michael Oliver <mcoliver@gmail.com>
2026-05-07 00:24:07 -0700

    fix: correctly alias vcpkg's Atomic_ops::atomic_ops target to atomic_ops::atomic_ops for compatibility
    Signed-off-by: Michael Oliver <mcoliver@gmail.com>

--- cmake/dependencies/atomic_ops.cmake
+++ cmake/dependencies/atomic_ops.cmake
@@ -7,14 +7,14 @@
 # Find the pre-compiled libatomic-ops provided by our vcpkg OpenUTVDeps environment
 FIND_PACKAGE(atomic_ops CONFIG REQUIRED)
 
-# The vcpkg port typically exports atomic_ops or atomic_ops::atomic_ops. We ensure atomic_ops::atomic_ops is available and global, as required by the GC module.
-IF(TARGET atomic_ops)
+# The vcpkg port exports Atomic_ops::atomic_ops (with a capital A). We ensure atomic_ops::atomic_ops is available and global, as required by the GC module.
+IF(TARGET Atomic_ops::atomic_ops)
   SET_PROPERTY(
-    TARGET atomic_ops
+    TARGET Atomic_ops::atomic_ops
     PROPERTY IMPORTED_GLOBAL TRUE
   )
   IF(NOT TARGET atomic_ops::atomic_ops)
-    ADD_LIBRARY(atomic_ops::atomic_ops ALIAS atomic_ops)
+    ADD_LIBRARY(atomic_ops::atomic_ops ALIAS Atomic_ops::atomic_ops)
   ENDIF()
 ELSEIF(TARGET atomic_ops::atomic_ops)
   SET_PROPERTY(
