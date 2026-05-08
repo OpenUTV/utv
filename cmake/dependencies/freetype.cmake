4e27e072b164bcec5cdcf04440543dad7a3ee5e4
Michael Oliver <mcoliver@gmail.com>
2026-05-08 11:10:24 -0700

    fix(freetype): supply explicit vcpkg hint to FIND_PATH
    Signed-off-by: Michael Oliver <mcoliver@gmail.com>

--- cmake/dependencies/freetype.cmake
+++ cmake/dependencies/freetype.cmake
@@ -12,9 +12,17 @@ IF(TARGET Freetype::Freetype)
     PROPERTY IMPORTED_GLOBAL TRUE
   )
 
+  # Derive the vcpkg root from the toolchain file to use as a hint
+  GET_FILENAME_COMPONENT(_vcpkg_scripts_dir "${CMAKE_TOOLCHAIN_FILE}" DIRECTORY)
+  GET_FILENAME_COMPONENT(_vcpkg_scripts "${_vcpkg_scripts_dir}" DIRECTORY)
+  GET_FILENAME_COMPONENT(_vcpkg_root "${_vcpkg_scripts}" DIRECTORY)
+
   # vcpkg's freetype-config.cmake occasionally fails to expose the proper include directory so we manually find the header and forcefully inject it into the
   # target.
-  FIND_PATH(FREETYPE_HACK_INC ft2build.h)
+  FIND_PATH(
+    FREETYPE_HACK_INC ft2build.h
+    HINTS "${_vcpkg_root}/installed/x64-windows/include"
+  )
   IF(FREETYPE_HACK_INC)
     SET_PROPERTY(
       TARGET Freetype::Freetype
