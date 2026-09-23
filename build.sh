94f2944436ed5d365c959ff196e8667f9276451f
Michael Oliver <mcoliver@gmail.com>
2026-09-23 14:46:56 -0700

    build(linux): add libaio-dev and X11 development headers for Ubuntu
    Signed-off-by: Michael Oliver <mcoliver@gmail.com>

--- build.sh
+++ build.sh
@@ -118,9 +118,10 @@ if [ "${INSTALL_DEPS}" -eq 1 ]; then
         if command -v apt-get >/dev/null 2>&1; then
             $SUDO apt-get update && DEBIAN_FRONTEND=noninteractive $SUDO apt-get install -y \
                 build-essential curl git patchelf \
-                libgl1-mesa-dev libglu1-mesa-dev libegl1-mesa-dev libosmesa6-dev libudev-dev \
+                libgl1-mesa-dev libglu1-mesa-dev libegl1-mesa-dev libosmesa6-dev libudev-dev libaio-dev \
                 libx11-dev libxcursor-dev libxext-dev libxi-dev libxinerama-dev \
-                libxrandr-dev libxrender-dev libxkbcommon-dev libxkbcommon-x11-dev \
+                libxrandr-dev libxrender-dev libxcomposite-dev libxdamage-dev libxtst-dev libxxf86vm-dev \
+                libxkbcommon-dev libxkbcommon-x11-dev libffi-dev \
                 libasound2-dev libpulse-dev
         fi
         brew install --formula \
