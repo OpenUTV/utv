7842510a7b2dbf20130188509aad5fcce73a972a
Michael Oliver <mcoliver@gmail.com>
2026-09-24 09:48:48 -0700

    fix(linux): link weak OpenGL fence and blit stubs in utvio and utv for LTO
    GCC 14 with -flto=auto requires symbol resolution for all references
    across the whole program. On Linux without proprietary NVIDIA drivers,
    Mesa does not export glFinishFenceNV, glSetFenceNV, glTestFenceNV,
    glGenFencesNV, glDeleteFencesNV, or glBlitFramebufferEXT.
    
    - Mark OpenGL fence and blit stubs in glfix.c as weak (__attribute__((weak)))
      and ensure glTestFenceNV returns GL_TRUE
    - Link glfix.c into utvio (src/bin/imgtools/rvio) and utv (src/bin/apps/rv)
      on Linux to satisfy LTO link requirements
    
    Signed-off-by: Michael Oliver <mcoliver@gmail.com>

--- src/bin/imgtools/rvio_sw/glfix.c
+++ src/bin/imgtools/rvio_sw/glfix.c
@@ -13,13 +13,19 @@
 #include <GL/gl.h>
 #include <GL/glext.h>
 
-void glBlitFramebufferEXT(GLint srcX0, GLint srcY0, GLint srcX1, GLint srcY1, GLint dstX0, GLint dstY0, GLint dstX1, GLint dstY1,
-                          GLbitfield mask, GLenum filter)
+#if defined(__GNUC__) || defined(__clang__)
+#define GL_WEAK __attribute__((weak))
+#else
+#define GL_WEAK
+#endif
+
+#ifdef PLATFORM_LINUX
+
+GL_WEAK void glBlitFramebufferEXT(GLint srcX0, GLint srcY0, GLint srcX1, GLint srcY1, GLint dstX0, GLint dstY0, GLint dstX1, GLint dstY1,
+                                  GLbitfield mask, GLenum filter)
 {
     glBlitFramebuffer(srcX0, srcY0, srcX1, srcY1, dstX0, dstY0, dstX1, dstY1, mask, filter);
 }
-
-#ifdef PLATFORM_LINUX
 
 //
 //  On linux, GL_NV_fence gets defined (not sure where), but the
@@ -27,15 +33,15 @@ void glBlitFramebufferEXT(GLint srcX0, GLint srcY0, GLint srcX1, GLint srcY1, GL
 //  actually need them, so provide the symbols here. -- Alan
 //
 
-void glSetFenceNV(GLuint u, GLenum e) {}
+GL_WEAK void glSetFenceNV(GLuint u, GLenum e) {}
 
-void glFinishFenceNV(GLuint u) {}
+GL_WEAK void glFinishFenceNV(GLuint u) {}
 
-void glGenFencesNV(GLsizei s, GLuint* ip) {}
+GL_WEAK void glGenFencesNV(GLsizei s, GLuint* ip) {}
 
-void glDeleteFencesNV(GLsizei s, const GLuint* ip) {}
+GL_WEAK void glDeleteFencesNV(GLsizei s, const GLuint* ip) {}
 
-GLboolean glTestFenceNV(GLuint u) {}
+GL_WEAK GLboolean glTestFenceNV(GLuint u) { return GL_TRUE; }
 
 #endif
 
@@ -45,12 +51,12 @@ GLboolean glTestFenceNV(GLuint u) {}
 //  Same as above but for AppleFence
 //
 
-void glSetFenceAPPLE(unsigned int u, unsigned int e) {}
+GL_WEAK void glSetFenceAPPLE(unsigned int u, unsigned int e) {}
 
-void glFinishFenceAPPLE(unsigned int u) {}
+GL_WEAK void glFinishFenceAPPLE(unsigned int u) {}
 
-void glGenFencesAPPLE(size_t s, unsigned int* ip) {}
+GL_WEAK void glGenFencesAPPLE(size_t s, unsigned int* ip) {}
 
-void glDeleteFencesAPPLE(size_t s, const unsigned int* ip) {}
+GL_WEAK void glDeleteFencesAPPLE(size_t s, const unsigned int* ip) {}
 
 #endif
